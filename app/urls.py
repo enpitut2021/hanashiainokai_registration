acfa0db9172c5483ebf58cda6a3cd58d88207243
shunnosuke <inoueshunnosuke@gmail.com>
2021-11-19 17:03:54 +0900

    make month schedule calender

--- app/urls.py
+++ app/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path('month_with_forms/<int:year>/<int:month>/', views.MonthWithFormsCalendar.as_view(), name='month_with_forms'),
     path('month', views.MonthCalendar.as_view(), name='month'),
     path('month/<int:year>/<int:month>/', views.MonthCalendar.as_view(), name='month'),  
+    path('month_with_schedule/',views.MonthWithScheduleCalendar.as_view(), name='month_with_schedule'),
+    path('month_with_schedule/<int:year>/<int:month>/',views.MonthWithScheduleCalendar.as_view(), name='month_with_schedule'),
 ]

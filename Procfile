423582993185e83600251430b90ed36a58d9897f
shiiiijp <82639092+shiiiijp@users.noreply.github.com>
2021-12-10 16:24:18 +0900

    Revert "Tobot"

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn hanasiainokai.wsgi --log-file -
-release: python manage.py migrate
--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 
 from .models import Schedule
 
-class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'summary', 'description', 'start_time', 'end_time', 'room', 'date', 'created_at')
-
-admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(Schedule)
 
 # Register your models here.

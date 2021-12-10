53d21904906a424310bcc50ba794da3a833a1f8b
Taishi Ito <itohlee14@gmail.com>
2021-12-10 16:02:03 +0900

    マイグレーション／adminでのSchedule管理

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Schedule
 
-admin.site.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'summary', 'description', 'start_time', 'end_time', 'room', 'date', 'created_at')
+
+admin.site.register(Schedule, ScheduleAdmin)
 
 # Register your models here.
